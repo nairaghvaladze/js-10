af8f6d436b94ce8d01c0e007187787b7b56ed1a4
nairaghvaladze <120264867+nairaghvaladze@users.noreply.github.com>
2023-05-08 13:19:50 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.body.style.backgroundColor="Burlywood";
+document.body.style.color="Darkmagenta";
+document.body.style.fontWeight="500";
+document.getElementById("text").innerHTML="რიცხვების მასივი 1000-დან 1-მდე!";
+document.getElementById("text").style.textAlign="center";
+document.getElementById("text").style.fontSize="24px";
+
+a=1000;
+while(a >= 2)
+{
+    document.write(a, " ");
+    a--;
+}
+
+
+
+
